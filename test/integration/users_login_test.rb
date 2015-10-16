f947c8e8cebe397eb542c77fac76cc5616d9e82b
abhijit kalta <abhijitkalta@gmail.com>
2015-10-16 14:38:52 +0530

    Log in page added

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -23,11 +23,21 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     get login_path
     assert_template 'sessions/new'
     post login_path, session: {email: @user.email, password: 'password'}
+    assert is_logged_in?
     assert_redirected_to @user
     follow_redirect!
     assert_template 'users/show'
     assert_select "a[href=?]", login_path, count: 0
     assert_select "a[href=?]", logout_path
     assert_select "a[href=?]", user_path(@user)
-  end
+    delete logout_path
+    assert_not is_logged_in?
+    assert_redirected_to root_url
+    follow_redirect!
+    assert_template 'static_pages/home'
+    assert_select "a[href=?]", root_path, count: 2
+    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", about_path
+    assert_select "a[href=?]", contact_path
+    end
 end
