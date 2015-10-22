87437b822d875bd202dba83da236fde751938df3
abhijit kalta <abhijitkalta@gmail.com>
2015-10-22 12:42:44 +0530

    Remember me option added

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-
+  attr_accessor :remember_token
   before_save { self.email = email.downcase }
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
@@ -17,5 +17,26 @@ class User < ActiveRecord::Base
     BCrypt::Password.create(string, cost: cost)
   end
 
-                                              
+  #returns a random token
+
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  #remembers a user in the database
+  def User.remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  #checks if remember token generates the same digest
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password? (remember_token)
+  end
+
+  #forgets a user
+  def forget
+    update_attribute(:remember_digest, nil)
+  end
 end
