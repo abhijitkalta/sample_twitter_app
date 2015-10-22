9e95cce74595ae817e9be04a10d450b801446787
abhijit kalta <abhijitkalta@gmail.com>
2015-10-23 03:38:02 +0530

    Finish user edit, update, delete and edit actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -25,7 +25,7 @@ module SessionsHelper
   end
 
   #returns true if user is logged in
-  def logged_in
+  def logged_in?
     !current_user.nil?
   end
 
@@ -42,4 +42,16 @@ module SessionsHelper
     session.delete(:user_id)
     @current_user = nil
   end
+
+  # Redirects to stored location (or to the default).
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # Stores the URL trying to be accessed.
+  def store_location
+    session[:forwarding_url] = request.url if request.get?
+  end
+
 end
